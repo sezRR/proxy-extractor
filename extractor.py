07bcf8948b7b888a3974fcf00f1a615c12f156a5
szRR <sezer.tetik2005@gmail.com>
2023-07-10 23:23:31 +0300

    code refactor and moved config initializer to new file

--- extractor.py
+++ extractor.py
@@ -1,10 +1,10 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from configparser import ConfigParser, ExtendedInterpolation
+from configparser import ConfigParser
 from pathlib import Path
 import numpy as np
 import pandas as pd
-import os
 from business_rule import BusinessRule
+from config_initializer import ConfigInitializer
 from exception_handler import ExceptionHandler
 
 
@@ -19,10 +19,7 @@ class Extractor:
         self.__read_data_file_path = self.__get_read_data_file_path()
 
     def __initialize_config(self) -> ConfigParser:
-        config = ConfigParser(interpolation=ExtendedInterpolation())
-        config.read("config.ini")
-
-        return config
+        return ConfigInitializer().initialize_config()
 
     def __get_read_data_file_path(self) -> str:
         return self.__config["PATH"]["proxy_lists_extract_from"]
@@ -39,13 +36,10 @@ class Extractor:
     def __read_csv_file(self, file_name, results) -> list[str]:
         content = pd.read_csv(file_name)
         content.fillna("", inplace=True)
-
         for column in content.columns:
             if "socks" in str(column):
-                proxy_list = content["socks4"].values if "4" in str(
-                    column) else content["socks5"].values
-                proxy_list = [
-                    f"socks{str(column).split('socks')[1]}://{proxy}" for proxy in proxy_list if len(proxy) > 1]
+                proxy_list = content["socks4"].values if "4" in str(column) else content["socks5"].values
+                proxy_list = [f"socks{str(column).split('socks')[1]}://{proxy}" for proxy in proxy_list if len(proxy) > 1]
                 results.extend(proxy_list)
             else:
                 proxy_list = content[column].values
@@ -56,15 +50,13 @@ class Extractor:
 
     def __read_txt_file(self, file_name, results) -> list[str]:
         content = np.loadtxt(file_name, dtype=str)
-        results.extend(
-            [proxy for proxy in content if proxy.strip().__len__() > 1])
+        results.extend([proxy for proxy in content if proxy.strip().__len__() > 1])
         return results
 
     def __separate_files(self, file_names) -> list[str]:  # type: ignore
         results = list()
         with ThreadPoolExecutor(10) as proxy_extractor_executor:
-            proxy_extractor_responses = [proxy_extractor_executor.submit(
-                self.__read_file, file_name, results) for file_name in file_names]
+            proxy_extractor_responses = [proxy_extractor_executor.submit(self.__read_file, file_name, results) for file_name in file_names]
 
         for future in as_completed(proxy_extractor_responses):
             return list(set(future.result()))  # type: ignore
