cc70d44e8bf42e00fda435c65b6d1acaa41e858a
Sezer Tetik <sezer.tetik2005@gmail.com>
2024-02-08 09:53:04 +0300

    ♻️ Project structure refactored.

--- config_initializer.py
+++
@@ -1,9 +0,0 @@
-from configparser import ConfigParser, ExtendedInterpolation
-
-
-class ConfigInitializer:
-    def initialize_config(self) -> ConfigParser:
-        config = ConfigParser(interpolation=ExtendedInterpolation())
-        config.read("config.ini")
-
-        return config
